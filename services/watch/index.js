38dd8f84c1c81c0fc053e552a91fd8261fa3ac3b
黃昀台 <teddy92729@gmail.com>
2023-07-27 22:37:47 +0800

    Add files via upload

--- services/watch/index.js
+++ services/watch/index.js
@@ -5,16 +5,18 @@ const ytdl=(os.platform()==="linux")?ytdlp:ytdlp.create(fileURLToPath(new URL(".
 
 export async function GET(req,res,url){
     let videoID=url.searchParams.get("v");
+    let time=url.searchParams.get("t");
     try{
-        if(req.headers.accept==="*/*"){
-                let video=ytdl.exec(`https://www.youtube.com/watch?v=${videoID}`,{
+        if(req.headers.accept==="*/*" && videoID){
+                let video=ytdl.exec(`https://www.youtube.com/watch?v=${videoID}`+(time)?`t=${time}`:"",{
                     output: "-",
                     format: "bv[height<=1100]+ba/b",
                     externalDownloader: (os.platform()==="linux")?"ffmpeg":fileURLToPath(new URL("./ffmpeg", import.meta.url)),
                     postprocessorArgs: "Merger+ffmpeg_i0:'-movflags +faststart -maxrate 5M -bufsize 10M',Merger+ffmpeg_o:'-movflags +faststart'",
                 });
                 
-                res.on("close",()=>{video.kill();});  
+                res.on("close",()=>{video.kill("SIGINT");});
+                
                 res.writeHead(200, {
                     "Content-Type": "video/webm",
                 });
@@ -23,7 +25,7 @@ export async function GET(req,res,url){
             res.writeHead(200, {
                 "Content-Type": "video/webm",
             });
-            res.end();           
+            res.end();
         }
     }catch(err){
         console.error(err);
