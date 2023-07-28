a42fd3fb42eb285d7afb21fc6cb5811f971cf3db
黃昀台 <teddy92729@gmail.com>
2023-07-29 01:33:44 +0800

    Add files via upload

--- services/index.js
+++ services/index.js
@@ -5,15 +5,14 @@ export function GET(req,res) {
     <html>
         <head>
             <meta charset="UTF-8">
-            <meta http-equiv=”refresh” content=”1;url=https://www.youtube.com/watch?v=dQw4w9WgXcQ” />
-            <link rel="preload" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+            <link rel="preload" href="https://www.youtube.com/embed/dQw4w9WgXcQ">
             <title>【我推的孩子】 [12] 線上看 - 巴哈姆特動畫瘋</title>
             <meta property="og:title" content="【我推的孩子】 [12] 線上看 - 巴哈姆特動畫瘋" />
             <meta property="og:type" content="video.movie" />
             <meta property="og:image" content="https://godlike-ahoge.ddns.net/0000128085.JPG" />
         </head>
         <body bgcolor="black">
-            <h1>載入中</h1>
+            <iframe width="1561" height="695" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" title="【我推的孩子】 [12] 線上看 - 巴哈姆特動畫瘋" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </body>
     </html>
     `);
