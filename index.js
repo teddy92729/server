3edf50064278dddcc6b3da38c98502db41ffcf63
黃昀台 <teddy92729@gmail.com>
2023-07-29 00:39:49 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,90 +1,67 @@
-import ytdlp  from "youtube-dl-exec";
-import {fileURLToPath} from "url";
-import os from "os";
-const ytdl=(os.platform()==="linux")?ytdlp:ytdlp.create(fileURLToPath(new URL("./yt-dlp", import.meta.url)));
+import { createReadStream } from "fs";
+import fs from "fs/promises";
+import mime from "mime";
+import http from "http";
+const http_port=5001;
 
-export async function GET(req,res,url){
-    let videoID=url.searchParams.get("v");
-    let type=url.searchParams.get("type");
-    if(req.headers.accept==="*/*"){
-            if(type!=="a"){
-                let video=ytdl.exec(`https://www.youtube.com/watch?v=${videoID}`,{
-                    output: "-",
-                    format: "bv[height<=1100]",
-                    externalDownloader: (os.platform()==="linux")?"ffmpeg":fileURLToPath(new URL("./ffmpeg", import.meta.url)),
-                    postprocessorArgs: "Merger+ffmpeg_i0:'-movflags +faststart -maxrate 5M -bufsize 10M',Merger+ffmpeg_o:'-movflags +faststart'",
-                });
-                video.on("error",(err)=>console.error(err));
-                res.on("close",()=>{video.kill("SIGINT");});
-                res.writeHead(200, {
-                    "Content-Type": "video/webm",
-                    "Connection": "close",
-                });
-                video.stdout.pipe(res);
-            }else{
-                let audio=ytdl.exec(`https://www.youtube.com/watch?v=${videoID}`,{
-                    output: "-",
-                    format: "ba",
-                    externalDownloader: (os.platform()==="linux")?"ffmpeg":fileURLToPath(new URL("./ffmpeg", import.meta.url)),
-                    postprocessorArgs: "Merger+ffmpeg_i0:'-movflags +faststart -maxrate 5M -bufsize 10M',Merger+ffmpeg_o:'-movflags +faststart'",
-                });
-                audio.on("error",(err)=>console.error(err));
-                res.on("close",()=>{audio.kill("SIGINT");});
-                res.writeHead(200, {
-                    "Content-Type": "audio/webm",
-                    "Connection": "close",
-                });
-                audio.stdout.pipe(res);
+const server=http.createServer(async function(req,res){
+    let url=new URL(req.url,`http://localhost:${http_port}`);
+    console.log(url);
+    let forceRes=true;
+    try{
+        const service = await import(`./services${url.pathname}/index.js`);
+        console.log(req.url+" OK(s)");
+        try{
+            await service[req.method](req,res,url);
+            forceRes=false;
+        }catch(err){
+            console.error(err);
+        }
+    }catch(err){
+        try{
+            // console.error(err);
+            // if(url.pathname.match(/index.js\/*$/i))throw new Error("index.js is not allow");
+            await fs.access(`./files${url.pathname}`,fs.constants.R_OK);
+            let {size}=await fs.stat(`./files${url.pathname}`);
+            let type=mime.getType(`./files${url.pathname}`);
+            console.log(req.url+" OK(f)");
+            try {
+                forceRes=false;
+                if (req.headers.range) {
+                    let [start,end] = req.headers.range.replace(/bytes=/, "").split("-");
+                    start = parseInt(start, 10);
+                    end = end ? parseInt(start, 10) : size-1;
+                    let chunksize = (end-start)+1;
+                    let ctype=type?{"Content-Type":type}:{};
+                    res.writeHead(206, {
+                        "Content-Range": "bytes " + start + "-" + end + "/" + size,
+                        "Accept-Ranges": "bytes", "Content-Length": chunksize,
+                        ...ctype
+                    });
+                    createReadStream(`./files${url.pathname}`,{start:start,end:end}).pipe(res);
+                 }else{
+                    res.writeHead(200);
+                    createReadStream(`./files${url.pathname}`).pipe(res);                    
+                }
+            } catch (err) {
+                console.error(err);
             }
-    }else if(videoID&&!type){
-        res.writeHead(200,{"Content-Type":"text/html"});
-        res.end(`
-            <html>
-                <head>
-                    <meta name="viewport" content="width=device-width">
-                </head>
-                <body bgcolor="black">
-                    <video id="playback_v" name="media" style="display: none;" autoplay="true" muted="true">
-                        <source src="${(new URL(`/watch?v=${videoID}&type=v`,url.href)).href}" type="video/webm">
-                    </video>
-                    <video id="playback_a" name="media" style="display: none;" autoplay="true" muted="true">
-                        <source src="${(new URL(`/watch?v=${videoID}&type=a`,url.href)).href}" type="video/webm">
-                    </video>
-                    <script type="text/javascript">
-                        let video=document.querySelector("#playback_v");
-                        let audio=document.querySelector("#playback_a");
-                        function readyState(videoElement){
-                            return new Promise((r)=>{
-                                const video=videoElement;
-                                if(video.readyState===4)
-                                  r(video);
-                                else
-                                  video.addEventListener("loadedmetadata",()=>{r(video);});
-                              });
-                        }
-                        video.pause();
-                        audio.pause();
-                        (async function(){
-                            await readyState(video);
-                            await readyState(audio);
-                            video.play();
-                            audio.play();
-                            audio.muted=false;
-                        })();
-                    </script>
-                    <script type="text/javascript" src="https://teddy92729.github.io/elementCreated.js"></script>
-                    <script type="text/javascript" src="https://pixijs.download/release/pixi.js"></script>
-                    <script type="text/javascript" src="https://teddy92729.github.io/anime4k_Deblur_DoG%20-%20test.js"></script>
-                </body>
-            </html>
-        `);
-    }else{
-        res.writeHead(200,{"Content-Type":"video/webm"});
-        res.end();
+        }catch(err){
+            console.error(err);
+            console.log(req.url+" Not Found");
+            res.writeHead(404);
+            res.end("404 Not Found");
+        }
+    }finally{
+        if(forceRes&&!res.writableEnded){
+            res.writeHead(500);
+            res.end("500 Internal Server Error");
+            console.log("500 Internal Server Error @"+url.pathname);
+        }
     }
-}
-
-export async function OPTIONS(req,res,url){
-    res.writeHead(200);
-    res.end();
-}
+});
+process.on('uncaughtException', function (err) {
+    console.error("UncaughtExecption: ", err); 
+});
+server.listen(http_port);
+console.log(`Server Start at port:${http_port}`);
